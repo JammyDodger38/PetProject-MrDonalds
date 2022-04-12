0c6c1bf57f7887b81bfab09c53b5a7673a19e303
Vlad Vladimirov <blad20002000@mail.ru>
2022-04-12 16:54:39 +0700

    preloader

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import dbMenu from '../DBMenu';
 import { ListItem } from './ListItem'
-import { Banner } from './Banner'
+import { Banner } from './Banner';
+import { useFetch } from '../Hooks/useFetch'
+import logoImg from '../../image/logo.svg'
 
 const MenuStyled = styled.main`
     background-color: #ccc;
@@ -12,24 +13,46 @@ const MenuStyled = styled.main`
 
 const SectionMenu = styled.section`
     padding: 30px;
-`
+`;
 
-export const Menu = ({ setOpenItem }) => (
-    <MenuStyled>
-        <Banner/>
-        <SectionMenu>
-            <h2>Бургеры</h2>
-            <ListItem 
-                itemList={dbMenu.burger}
-                setOpenItem={setOpenItem}
-            />
-        </SectionMenu>
-        <SectionMenu>
-            <h2>Закуски / Напитки</h2>
-            <ListItem 
-                itemList={dbMenu.other}
-                setOpenItem={setOpenItem}
-            />
-        </SectionMenu>
-    </MenuStyled>
-);
+const Preloader = styled.img`
+    width: 50px;
+    height: 50px;
+    animation: rotate 2s linear infinite;
+    @keyframes rotate{
+        100%{
+         transform: rotate(360deg)
+        }
+`;
+
+export const Menu = ({ setOpenItem }) => {
+
+    const res = useFetch();
+    const dbMenu = res.response;
+
+    return (
+        <MenuStyled>
+            <Banner/>
+            {res.response ?
+                <>
+                    <SectionMenu>
+                        <h2>Бургеры</h2>
+                        <ListItem 
+                            itemList={dbMenu.burger}
+                            setOpenItem={setOpenItem}
+                        />
+                    </SectionMenu>
+                    <SectionMenu>
+                        <h2>Закуски / Напитки</h2>
+                        <ListItem 
+                            itemList={dbMenu.other}
+                            setOpenItem={setOpenItem}
+                        />
+                    </SectionMenu>
+                </> : res.error ?
+                <div>Sorry, we will fix it soon...</div> :
+                <Preloader src={logoImg}/>
+            }
+        </MenuStyled>
+    )
+};
